7d41b7f69e41b8798723549f6912333ce17c490c
jean11 <jean.boudet@yahoo.fr>
2014-06-23 08:48:13 +0200

    Ajout des méthodes send et handle HttpResponse

--- src/Core/Http/HttpResponse.php
+++ src/Core/Http/HttpResponse.php
@@ -5,11 +5,43 @@ use Core\Controller\ResolverController;
 
 class HttpResponse
 {
+	/**
+	 * Référence au ResolverController
+	 *
+	 * @var Core\Controller\ResolverController
+	 */
 	private $resolveController;
 
+	/**
+	 * Crée une nouvelle instance
+	 *
+	 * @param Core\Controller\ResolverController $resolveController
+	 * @return void
+	 */
 	public function __construct(ResolverController $resolveController)
 	{
 		$this->resolveController = $resolveController;	
-		var_dump($this->resolveController);
+	}
+
+	/**
+	 * Envoit la page au navigateur
+	 *
+	 */
+	public function send()
+	{
+			
+	}
+
+	/**
+	 * Effectue le traitement
+	 */
+	private function handle()
+	{
+		$object = $this->resolveController->getInstanceController();
+		$t = call_user_func_array(array(
+			$object, 
+			$this->resolveController->getMethodName()),
+			$this->resolveController->getMethodParameters()
+		)
 	}
 }
