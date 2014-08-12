cb6daf4b31f64a7e384c94ebbd29977a351fbc17
jean11 <jean.boudet@yahoo.fr>
2014-08-12 20:45:59 +0200

    Modification UrlCreator

--- src/Core/Bracket/Twig/UrlCreator.php
+++ src/Core/Bracket/Twig/UrlCreator.php
@@ -3,8 +3,19 @@ namespace Core\Bracket\Twig;
 
 class UrlCreator
 {
+	/**
+	 * Fonction pour Twig permettant
+	 * de générer une url quelque soit
+	 * le server
+	 *
+	 * @param  string $uri
+	 * @return string
+	 */
 	public function create($uri)
 	{
-		print_r($_SERVER);
+		$host = $_SERVER['HTTP_HOST'];
+		$scriptName = $_SERVER['SCRIPT_NAME'];
+		$url = 'http://'.$host.preg_replace('/index.php/','',$scriptName).$uri;
+		return $url;
 	}
 }
